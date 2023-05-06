46059c00f901c76687827087399489cbf1f2eb44
mikan19 <mellamo.rina.ty@gmail.com>
2023-05-06 17:09:59 +0900

    feat: 支出の低い順に一覧表示しました

---
+++ src/public/spendings.php
@@ -0,0 +1,36 @@
+<?php
+
+$dbUserName = "root";
+$dbPassword = "password";
+$spendingReport = new outputSpending($dbUserName, $dbPassword);
+$spendingReport->printSpendings();
+
+
+class outputSpending {
+  private $pdo;
+
+  public function __construct($dbUserName, $dbPassword) {
+    $this->pdo = new PDO("mysql:host=mysql; dbname=tq_quest; charset=utf8", $dbUserName, $dbPassword);
+  }
+
+  public function printSpendings() {
+    $sql = "SELECT * FROM spendings";
+    $statement = $this->pdo->prepare($sql);
+    $statement->execute();
+    $spendings = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    $sort = [];
+    foreach($spendings as $spendingKey => $spending) {
+      $sort[$spendingKey] = $spending["amount"];
+    }
+
+    array_multisort($sort, SORT_ASC, $spendings);
+
+    foreach($spendings as $spendingBySort) {
+      echo $spendingBySort["amount"];
+      echo "<br />";
+    }
+  }
+}
+
+
